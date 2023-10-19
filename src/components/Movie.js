0b77708aeb876bc34d4e54701fe9e47ce8065c15
Hyoseo Lee <hyoseo837@gmail.com>
2023-10-19 18:39:28 -0400

    last commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ function Movie({ id, mediumCoverImage, title, summary, genres }) {
     <div>
       <img src={mediumCoverImage} alt="Unavailable" />
       <h2>
-        <Link to={"/movie/" + id}>{title}</Link>
+        <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
